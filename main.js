c1213aebc3075d07976454b3b9c3449f2ebf10a5
Gentille Plume <melvin.lemoine@feather-project.org>
2018-06-21 17:55:31 +0200

    Added a sentence in the message

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 setTimeout(function(){
   var typed = new Typed('#typed', {
-      strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'féministe', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'un visionnaire', 'amoureux des plantes', 'différent', 'serviable', 'ambitieux', 'ce que je suis' ],
+      strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'féministe', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'un visionnaire', 'amoureux des plantes', 'différent', 'serviable', 'ambitieux', 'militant pour un monde meilleur', 'ce que je suis' ],
       typeSpeed: 75,
       backDelay: 3000,
       fadeOut: true,
       loop: true
   });  
-}, 5000);  
+}, 5000);
 
