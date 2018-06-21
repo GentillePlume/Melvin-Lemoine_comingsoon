782ad1e487de94e43cef2122dd5069135aae813a
Gentille Plume <melvin.lemoine@feather-project.org>
2018-06-21 18:17:05 +0200

    Animated the message

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-setTimeout(function(){
-  var typed = new Typed('#typed', {
-      strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'féministe', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'un visionnaire', 'amoureux des plantes', 'différent', 'serviable', 'ambitieux', 'militant pour un monde meilleur', 'ce que je suis' ],
-      typeSpeed: 75,
-      backDelay: 3000,
-      fadeOut: true,
-      loop: true
-  });  
-}, 5000);
+setTimeout(function () {
+    $("#me")[0].style.display = "inline-block";
+    setTimeout(function () {
+        $("#me")[0].style.animationName = "Iam";
+    }, 1050);
+}, 1000);
 
+setTimeout(function () {
+    var typed = new Typed('#typed', {
+        strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'web designer', 'féministe', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'visionnaire', 'compositeur', 'amoureux des plantes', 'différent', 'serviable', 'ambitieux', 'militant pour un monde meilleur', 'ce que je suis'],
+        typeSpeed: 75,
+        backDelay: 3000,
+        fadeOut: true,
+        loop: true
+    });
+}, 2500);
