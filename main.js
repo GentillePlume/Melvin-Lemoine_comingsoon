03a66c7db9c821aada9dbc3a543ce7da329fa295
Gentille Plume <melvin.lemoine@feather-project.org>
2018-06-21 17:48:22 +0200

    Developed the typed element

---
+++ main.js
@@ -0,0 +1,7 @@
+  var typed = new Typed('#typed', {
+      strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'féministe', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'un visionnaire', 'amoureux des plantes', 'différent', 'serviable', 'ambitieux', 'Melvin' ],
+      typeSpeed: 75,
+      backDelay: 3000,
+      fadeOut: true,
+      loop: true
+  });
