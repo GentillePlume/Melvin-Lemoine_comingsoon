1abfd0fd927f1de9302f29e8b8b55ad182685111
Gentille Plume <melvin.lemoine@feather-project.org>
2018-07-14 21:50:01 +0200

    Minor typo correction

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ setTimeout(function () {
 
 setTimeout(function () {
     var typed = new Typed('#typed', {
-        strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'web designer', 'allié au féministe', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'visionnaire', 'compositeur', 'amoureux des plantes', 'ambitieux', 'militant', 'ce que je suis', 'Melvin'],
+        strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'web designer', 'allié au féminisme', 'perfectionniste', 'vegan', 'sportif', 'foufou', 'visionnaire', 'compositeur', 'amoureux des plantes', 'ambitieux', 'militant', 'ce que je suis', 'Melvin'],
         typeSpeed: 75,
         backDelay: 3000,
         fadeOut: true,
