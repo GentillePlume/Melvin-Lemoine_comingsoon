17c46902ddef7f01cc91e610144127c6edb9b1c2
Gentille Plume <melvin.lemoine@feather-project.org>
2018-09-21 13:16:24 +0200

    Cleared all the projet to work on the second idea

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// ##################################################
+//                    TYPED.JS
+// ##################################################
+
+setTimeout(function() {
+  var typed = new Typed('#typed', {
+    strings: ['développeur web', 'graphiste', 'entrepreneur', 'photographe', 'vidéaste', 'musicien', 'web designer', 'allié au féminisme', 'perfectionniste', 'vegan', 'sportif', 'inspirateur', 'foufou', 'visionnaire', 'compositeur', 'amoureux des plantes', 'ambitieux', 'militant', 'ce que je suis', 'Melvin'],
+    typeSpeed: 75,
+    backDelay: 3000,
+    fadeOut: true,
+    loop: true
+  });
+}, 2500);
