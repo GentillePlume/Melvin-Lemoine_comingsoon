c63b9d0c1433e1afa58463240a99dfa1a566e026
GentillePlume <melvin.lemoine@feather-project.org>
2018-09-26 14:28:16 +0200

    Developed the first part of the button

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,40 @@ setTimeout(function() {
 for (i = 0; i < $(".home__style").length; i++) {
   $(".home__style")[i].style.animationDuration = Math.floor((Math.random() * 15) + 5) + "s";
 }
+
+// ##################################################
+//              CHANGE PAGE FUNCTION
+// ##################################################
+
+$("header").click(function() {
+
+  for (i = 0; i < $(".home__style").length; i++) {
+    $(".home__style")[i].style.opacity = 0;
+  }
+
+  // NAVIGATION BAR
+  $("#networks")[0].style.animationName = "fadeOutDown";
+
+  // PROFIL CONTENT
+  $(".profil__image")[0].style.animationName = "fadeOut";
+  $(".profil__name")[0].style.animationName = "fadeOutDown";
+  $(".profil__desc")[0].style.animationName = "fadeOutDown";
+
+  // STATUS BOX
+  $(".status__line")[0].style.animationName = "fadeOutDown";
+  $(".status__image")[0].style.animationName = "bounceOut";
+  $(".status__title")[0].style.animationName = "fadeOutRight";
+  $(".status__desc")[0].style.animationName = "fadeOutRight";
+  setTimeout(function() {
+    $("#status")[0].style.display = "none";
+
+    for (i = 0; i < $(".home__style").length; i++) {
+      $(".home__style")[i].style.display = "none";
+    }
+  }, 1000);
+
+  // HEADER
+  $(".header__img")[0].style.animationName = "fadeOut";
+  $(".header__p")[0].style.animationName = "fadeOutUp";
+
+})
